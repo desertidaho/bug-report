635395e838fc2f1d9b556b43249c3d1d6f6ee606
Brett Wilcox <wilcox.brett@gmail.com>
2019-03-08 13:21:01 -0700

    Initial file/folder setup.

---
+++ src/views/Details.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="details ">
+      
+   </div>
+</template>
+
+<script>
+export default {
+   name: "details",
+   props: [],
+   data() {
+      return {}
+   },
+   computed: {},
+   methods: {},
+   components: {}
+}
+</script>
